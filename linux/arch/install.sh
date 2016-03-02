8c8b38b2f0e4e3c763b93ff1d18a3488c8e4654b
Daniel S <standeo@gmail.com>
2016-03-03 10:25:04 +1100

    Do not confirm when installing packages

--- linux/arch/install.sh
+++ linux/arch/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pacman -Sy curl
-pacman -Sy wget
-pacman -Sy git
-pacman -Sy python3
-pacman -Sy vim
-pacman -Sy zsh
+pacman -Sy --noconfirm curl
+pacman -Sy --noconfirm wget
+pacman -Sy --noconfirm git
+pacman -Sy --noconfirm python3
+pacman -Sy --noconfirm vim
+pacman -Sy --noconfirm zsh
