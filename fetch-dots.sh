326e48476446ff94eef81d9d1ec8226abd1f4936
Daniel S <standeo@gmail.com>
2016-03-03 12:47:47 +1100

    Make setup dots smarter

--- fetch-dots.sh
+++
@@ -1,5 +0,0 @@
-echo "Getting zshrc..."
-curl https://raw.githubusercontent.com/dzotokan/dotfiles/master/zshrc > $HOME/.zshrc
-
-echo "Getting tmux config..."
-curl https://raw.githubusercontent.com/dzotokan/dotfiles/master/tmux.conf > $HOME/.tmux.conf
---
+++ setup-dots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+
+DOTFILES=$HOME/.dotfiles
+
+function unlinkOrMove() {
+  local path=$1
+  #TODO: check for soft links
+  [ -f $path ] && mv -i $path{,.bak}
+}
+
+if [ -d $DOTFILES ]
+then
+  echo "$DOTFILES already initialized"
+  #TODO: ask for a re-link?
+else
+  echo "Fetching dots..."
+  git clone git@github.com:dzotokan/dotfiles.git $DOTFILES
+
+  #TODO: map over a list of files
+  echo "Linking zshrc..."
+  unlinkOrMove "$HOME/.zshrc"
+  ln $DOTFILES/zshrc $HOME/.zshrc
+
+  echo "Linking tmux conf..."
+  unlinkOrMove "$HOME/.tmux.conf"
+  ln $DOTFILES/tmux.conf $HOME/.tmux.conf
+fi
+
+
+
