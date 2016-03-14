7f4d6963a13186ad56bfb4f16bee84677bf4d9ee
Daniel S <standeo@gmail.com>
2016-03-14 21:21:07 +1100

    Add global gitignore

--- common/setup-git.sh
+++ common/setup-git.sh
@@ -19,3 +19,7 @@ git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.interactive auto
 git config --global color.status auto
+
+### Use global gitignore
+git config --global core.excludesfile ~/.gitignore
+
