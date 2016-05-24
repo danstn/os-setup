c1e8c89aca7c2e3c44d988d9e07b93a5ca32997a
Daniel S <standeo@gmail.com>
2016-05-25 09:11:10 +1000

    Add docker to arch installer

--- linux/arch/install.sh
+++ linux/arch/install.sh
@@ -6,3 +6,6 @@ pacman -Sy --noconfirm git
 pacman -Sy --noconfirm python3
 pacman -Sy --noconfirm vim
 pacman -Sy --noconfirm zsh
+pacman -Sy --noconfirm ghc cabal-install haddock happy alex
+pacman -Sy --noconfirm docker
+
